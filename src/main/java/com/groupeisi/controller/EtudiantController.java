bda156cf57834fbee9ac71fade35dcdd2a6f55af
M-N-21 <modyndiaye416@gmail.com>
2024-02-29 00:26:51 +0000

    update with capture

--- src/main/java/com/groupeisi/controller/EtudiantController.java
+++ src/main/java/com/groupeisi/controller/EtudiantController.java
@@ -64,7 +64,7 @@ public class EtudiantController {
 
     }
 
-    @PostMapping("/etudiants")
+    @PostMapping("/alletudiants")
     public ResponseEntity<Object> getAllEtudiant(@RequestBody String query) {
         ExecutionResult result = graphQL.execute(query);
         return new ResponseEntity<Object>(result, HttpStatus.OK);
